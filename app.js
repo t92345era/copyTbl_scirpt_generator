e4b69b0bf344fc4d0c1fe4103a937314787d11c4
tera <terasute0321@gmail.com>
2020-09-30 10:15:29 +0900

    update: js処理

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ new Vue({
   el: "#app",
   data: {
     copy_from: "",
-    copy_to: ""
+    copy_to_input: "",
+    mode: "yyyymmdd",
+    yyyy: "",
+    yy: "",
   },
   methods: {
     sayHello() {
@@ -10,6 +13,13 @@ new Vue({
     }
   },
   computed: {
+    copy_to: function() {
+      if (this.mode == 'input') return this.copy_to_input;      
+      if (this.mode == 'yyyymmdd') return this.copy_from + "_" + this.yyyy;
+      if (this.mode == 'yymmdd') return this.copy_from + "_" + this.yy;
+      return this.copy_to_input;
+    },
+
     sqlServer: function () {
       return `SELECT * INTO ${this.copy_to} FROM ${this.copy_from};`;
     },
@@ -21,9 +31,20 @@ new Vue({
     },
     mySQL: function() {
       return `CREATE TABLE ${this.copy_to} SELECT  FROM ${this.copy_from};`;
+    },
+    sqLite: function() {
+      return `CREATE TABLE ${this.copy_to} AS SELECT  FROM ${this.copy_from};`;
     }
   },
   mounted() {
-    //表示後にやりたいことはここに書ける
+    var dt = new Date();
+    var fYear = ( '0000' + dt.getFullYear() ).slice( -4 );
+    var sYear = ( '' + dt.getFullYear() ).slice( -2 );
+    var m = ( '' + dt.getMonth() + 1 ).slice( -2 );
+    var d = ( '' + dt.getDate() ).slice( -2 );
+
+    this.yyyy = fYear + m + d;
+    this.y = sYear + m + d;
   }
+  
 })
